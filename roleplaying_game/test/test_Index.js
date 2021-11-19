62c71f96ec6ea1ca22121541e3bdad79d9553cf7
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-11-18 23:07:58 -0800

    11/18/21

--- roleplaying_game/test/test_Index.js
+++ roleplaying_game/test/test_Index.js
@@ -1,3 +1,13 @@
 const rolePlayingGame = require("../Index");
 const assert = require("chai").assert;
-describe("RolePlayingGame", ()=>{})
+describe("RolePlayingGame", () => {
+    it("Can create an instance of the RolePlayingGame", () => {
+        let game = new rolePlayingGame();
+        assert.isDefined(game.player, "{}");
+    });
+    it("Can Create an Item", () => {
+        let game = rolePlayingGame.gameGrabber.Item()
+        let item = new game("Mandolorian Jetpack", "Grants Flight and Speed Boost, Allowing for multiple attacks per turn", 1000)
+        assert.equal(item, )
+    })
+});

ca0899863b622fc28e38b84c9185f1677580fa00
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2022-01-22 12:15:53 -0800

    1/22/2022

--- madlib_generator/madlib.js
+++ madlib_generator/madlib.js
@@ -4,9 +4,6 @@ let town = readlineSync.question("Town:");
 let verb_one = readlineSync.question("Verb:");
 let verb_two = readlineSync.question("Verb:");
 let adjective = readlineSync.question("Adjective:");
-function madlib() {
-  console.log(
-    `Hello, My name is ${celebrity}! I live in ${town}. I have a ${adjective} cat, he likes to ${verb_one} all over me. we go ${verb_two}ing often and I can tell he loves it.`
-  );
-}
-madlib();
+console.log(
+  `Hello, My name is ${celebrity}! I live in ${town}. I have a ${adjective} cat, he likes to ${verb_one} all over me. we go ${verb_two}ing often and I can tell he loves it.`
+);

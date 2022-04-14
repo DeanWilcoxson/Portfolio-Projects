cbd3abfd04996e14a810a683976d1fc569e0e8bb
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2022-04-14 11:03:03 -0700

    4/14/22

--- portfolio/src/components/Projects/ProjectAesthetics.js
+++ portfolio/src/components/Projects/ProjectAesthetics.js
@@ -1,18 +1,25 @@
 import styled from "styled-components";
 import { FaGithub } from "react-icons/fa";
-export const ProjectContainer = styled.div`
-  margin: 2rem;
-  padding: 0 5rem;
-  width: 80%;
-  box-shadow: grey 8px 8px 16px;
-  border-radius: 10px;
+export const ProjectBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   @media screen and (max-width: 768px) {
-    width: 60%;
-    text-align: center;
+    justify-content: flex-start;
+    align-items: flex-start;
   }
   @media screen and (max-width: 370px) {
-    width: 40%;
+    justify-content: flex-start;
+    align-items: flex-start;
   }
+`;
+export const ProjectTopLine = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 export const ProjectTitle = styled.h1`
   text-shadow: 1.5px 1px 2px grey;
@@ -35,34 +42,30 @@ export const GithubIcon = styled(FaGithub)`
     cursor: pointer;
   }
 `;
-export const ProjectTopLine = styled.div`
-  width: 90%;
+export const ProjectImage = styled.img`
+  align-self: center;
+  border-radius: 5px;
+`;
+export const ProjectListContainer = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`;
-export const ProjectBox = styled.div`
-  display: flex;
-  flex-direction: column;
+  width: 90%;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
+`;
+export const ProjectContainer = styled.div`
+  margin: 2rem;
+  padding: 0 5rem;
+  width: 80%;
+  box-shadow: grey 8px 8px 16px;
+  border-radius: 10px;
   @media screen and (max-width: 768px) {
-    justify-content: flex-start;
-    align-items: flex-start;
+    width: 60%;
+    text-align: center;
   }
   @media screen and (max-width: 370px) {
-    justify-content: flex-start;
-    align-items: flex-start;
+    width: 40%;
   }
-`;
-export const ProjectList = styled.ul`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-  align-items: center;
-  text-align: center;
 `;
 export const ProjectListBox = styled.div`
   width: 100%;
@@ -72,15 +75,16 @@ export const ProjectListBox = styled.div`
   align-items: center;
   flex-wrap: wrap;
 `;
-export const ProjectListContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 90%;
-  justify-content: center;
-  align-items: flex-start;
-`;
 export const ProjectListDesc = styled.h4`
   text-shadow: 0.75px 1px 1.5px grey;
+`;
+export const ProjectList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: center;
+  text-align: center;
 `;
 export const ProjectItem = styled.li`
   font-size: small;
@@ -96,8 +100,4 @@ export const ProjectItem = styled.li`
   @media screen and (max-width: 370px) {
     font-size: x-small;
   }
-`;
-export const ProjectImage = styled.img`
-  align-self: center;
-  border-radius: 5px;
 `;
